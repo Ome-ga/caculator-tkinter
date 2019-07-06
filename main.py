b60612f309e5c783a881350e2304c521e362fd77
Omega <fuhm@raysdate.com>
2019-07-06 23:39:27 +0800

    2343

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 #If any advice let me know dk2086@126.com
 
 import tkinter as TK
+from tkinter import *
 # 主窗口
 root = TK.Tk( )                     # 创建TK实例
 root.title("King's Caculator")      # 设置窗口的显示名字
@@ -19,8 +20,22 @@ equation.set('0')                      # 赋初始值
 
 # 结果显示框
 show_uresult = TK.Label(root,bg='white',fg = 'black',font =('Arail','15'),bd='0',textvariable =equation,anchor='se')
-show_dresult = TK.Label(root,bg='white',fg = 'black',font = ('Arail','30'),bd='0',textvariable=result,anchor='se')
+show_dresult = TK.Label(root,bg='lightblue',fg = 'black',font = ('Arail','30'),bd='0',textvariable=result,anchor='se')
 show_uresult.place(x='10',y='10',width='300',height='50')
+show_dresult.place(x='10',y='60',width='300',height='50')
+
+
+# 按钮
+# 第一行按钮
+button_back =TK.Button(root,text='←',bg='DarkGray',command=back)
+button_back.place(x = '10',y='150',width = '60',height='40')
+button_lbracket=TK.Button(root,text='(',bg='DarkGray',command= lambda : getnum('('))
+button_lbracket.place(x = '90',y='150',width = '60',height='40')
+button_rbracket=TK.Button(root,text=')',bg='DarkGray',command= lambda : getnum(')'))
+button_rbracket.place(x = '170',y='150',width = '60',height='40')
+button_division =TK.Button(root,text='÷',bg='DarkGray',command= lambda : getnum('÷'))
+button_division.place(x = '250',y='150',width = '60',height='40')
+
 
 
 root.mainloop( )
